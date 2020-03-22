320cd59df97e919fb1c9d090e96574850ee32d4f
Mauri Moretti <moretti.juniorz@gmail.com>
2020-03-22 12:35:32 -0300

    adding environment variables

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,11 @@
+require('dotenv/config');
+
 module.exports = {
     dialect: 'mysql',
-    host: 'localhost',
-    username: 'admin',
-    password: 'admin',
-    database: 'gobarber',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     define: {
         timestamps: true,
         underscored: true,
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 module.exports = {
-    dsn: 'https://1e521355f8fd41b797778421173aba66@sentry.io/5170716'
+    dsn: process.env.SENTRY_DSN
 };
